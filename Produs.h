56bfd6522eac7a55e482a02d1e12375a8de99cd2
Bananutzul <zamhoria15@gmail.com>
2026-04-18 13:40:58 +0300

    Added Product header file

---
+++ Produs.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Product {
+protected:
+    string name;
+    double price;
+    int quality; // will be used to calculate effectiveness of potion and price, from 0 - 10
+
+public:
+    Product();
+    Product(string name, double price, int quality);
+    Product(const Product& prod);
+    Product& operator=(const Product& prod);
+    virtual ~Product();
+
+    virtual double addToPotion() = 0;
+    virtual double calculatePrice();
+
+    friend ostream& operator<<(ostream &os, const Product& prod);
+};
